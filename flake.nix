aafbbd87c85a934e2ed3ec75f70dcb5a34629a2b
Loïc Reynier <loic@loicreynier.fr>
2025-01-15 22:36:39 +0100

    build(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   description = "SkyrimSE mod dev environment for my misc patches";
 
-  outputs = { self, flake-utils, nixpkgs, git-hooks, pandoc-bbcode_nexus, ... }:
-    (flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      git-hooks,
+      pandoc-bbcode_nexus,
+      ...
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        pandocBBCodeNexus =
-          pandoc-bbcode_nexus.packages.${system}.pandoc-bbcode_nexus;
-      in {
+        pandocBBCodeNexus = pandoc-bbcode_nexus.packages.${system}.pandoc-bbcode_nexus;
+      in
+      {
         checks = {
           pre-commit-check = git-hooks.lib.${system}.run {
             src = ./.;
@@ -20,7 +29,7 @@
               editorconfig-checker.enable = true;
               prettier.enable = true;
               markdownlint.enable = true;
-              nixfmt.enable = true;
+              nixfmt-rfc-style.enable = true;
               statix.enable = true;
               typos.enable = true;
             };
@@ -29,21 +38,18 @@
 
         devShells.default = pkgs.mkShell {
           inherit (self.checks.${system}.pre-commit-check) shellHook;
-          packages = with pkgs; [ just pandocBBCodeNexus zip ];
+          packages = with pkgs; [
+            just
+            pandocBBCodeNexus
+            zip
+          ];
         };
-      }));
+      }
+    ));
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
-    nixpkgs-lor = {
-      url = "github:loicreynier/nixpkgs-lor";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-        git-hooks.follows = "git-hooks";
-      };
-    };
     git-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
